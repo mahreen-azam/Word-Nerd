1f0c510c0c935268a0213ad077e6336ea27c410e
Mahreen <azam03m@gmail.com>
2019-10-17 00:09:40 -0400

    Added response handlers for get word of the response

--- Word Nerd/Word Nerd/Controllers/DailyWordViewController.swift
+++ Word Nerd/Word Nerd/Controllers/DailyWordViewController.swift
@@ -22,24 +22,51 @@ class DailyWordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Call randomWord endpoint and update labels to be today's word and definiton. If no word is returned, show an error message
+        WordnikClient.getWordOfTheDay(completion: handleWordOfDayResponse(success:error:))
     }
     
    // MARK: Button Functions
     @IBAction func saveWordTapped(_ sender: Any) {
+        print("save word tapped")
         // Need to pass current word and definition to the next view controller. User should be allowed to select a list. If no lists exist, let them create a new list and afterwards they should be able to save that word into that list if they tap on it. (have global variable that hold the word and definition variables. Once they are succesffully saved into a notebook, clear the variables to be nil.
     }
     
     @IBAction func newWordTapped(_ sender: Any) {
+        print("new word tapped")
         // Call "randomWord" endpoint. Save word. Call "Search" endpoint for that randomWord. Update UI for word and definition IF a success is returned. Else, show error and do not update UI.
     }
     
     @IBAction func searchButtonTapped(_ sender: Any) {
-        // Show an alera (?) that allows the user to enter the word that they want to search.
+        print("search tapped")
+        // Show an alert (?) that allows the user to enter the word that they want to search.
         //  Call "Search" endpoint for the user's endpoint. Update UI for word and definition IF a success is returned. Else, show error and do not update UI.
     }
     
+    // MARK: Response Handlers
+    func handleWordOfDayResponse(success: WordOfTheDay?, error: Error?) {
+        DispatchQueue.main.async {
+            // self.setLoggingIn(false)
+            if success != nil {
+                self.wordLabel.text = success!.word
+                self.definitionLabel.text = success!.definitions[0].text
+                
+            } else {
+                self.showFailure(title: "Failed to get Word of the Day", message: error?.localizedDescription ?? "")
+            }
+        }
+    }
+    
+    // MARK: Error Handlers
+    func showFailure(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertVC, animated: true)
+    }
+    
 }
+
+
+// Extra things to come back to: Make buttons with rounded corners, set up new word button, setup search 
 
 
 
